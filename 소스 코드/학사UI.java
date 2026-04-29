d86351cc2a2c00861c32f76f3add72dcf54bc11e
JAW1006 <202245027@itc.ac.kr>
2026-04-29 16:32:53 +0900

    최진호
    소스코드 업로드 완료했습니다

---
+++ 소스 코드/학사UI.java
@@ -0,0 +1,11 @@
+package haksa;
+
+public class 학사UI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		과목 _sub = new 과목();
+		System.out.println("학점 :"+_sub.과목점수입력("kim", "1234", "sub001", 85, "stu001"));
+	}
+
+}
